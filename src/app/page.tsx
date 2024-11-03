e0676789a55c57021e74f1b9eadc6a81c2048934
Mathys <mathys0@hotmail.fr>
2024-11-03 21:51:26 +0100

    1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,27 +2,25 @@
 
 import { useEffect, useState } from "react";
 
-// Définir l'interface pour le message
 interface Message {
   id: number;
-  message: string; // Assurez-vous que cela correspond à la structure de votre objet
+  message: string; 
 }
 
-// Définir la base URL
 const BASE_URL = 'https://express-back-end-phi.vercel.app';
 
 export default function Home() {
-  const [messages, setMessages] = useState<Message[]>([]); // Utiliser l'interface Message ici
-  const [error, setError] = useState<string | null>(null); // Définir le type pour l'erreur
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [error, setError] = useState<string | null>(null); 
 
   useEffect(() => {
     const fetchMessages = async () => {
       try {
-        const response = await fetch(`${BASE_URL}/mathys`); // Utiliser la base URL ici
+        const response = await fetch(`${BASE_URL}/mathys`); 
         if (!response.ok) {
           throw new Error('Erreur lors de la récupération des messages');
         }
-        const data: Message[] = await response.json(); // Typage de la réponse
+        const data: Message[] = await response.json();
         setMessages(data);
       } catch (err) {
         setError(err instanceof Error ? err.message : 'Une erreur inconnue est survenue');
